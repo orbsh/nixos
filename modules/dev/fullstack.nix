05a958c49f4fe9716c2bf8ef46ec8d9a812b5d80
fj0r <fj0rd@qq.com>
2026-05-30 18:10:49 +0800

    Add k8s and wasm modules to fullstack dev config

--- modules/dev/fullstack.nix
+++ modules/dev/fullstack.nix
@@ -6,6 +6,8 @@
     ./units/haskell.nix
     ./units/data-tools.nix
     ./units/net-tools.nix
+    ./units/k8s.nix
+    ./units/wasm.nix
     # 服务端（按需：容器化/独立部署场景取消注释）
     # ./units/mysql-server.nix
     # ./units/postgresql-server.nix
