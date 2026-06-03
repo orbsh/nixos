170ff2db0bcf8f9df2e52ea789a71e59b0527d41
fj0r <fj0rd@qq.com>
2026-06-03 13:59:24 +0800

    refactor: 移除中文 locale 覆盖，简化 ADR-001 文档语法
    - 移除 LC_ALL=zh_CN.UTF-8 设置，系统完全使用英文 locale
    - ADR-001 文档同步更新为简化后的 fetchTree 语法

--- modules/system/units/base.nix
+++ modules/system/units/base.nix
@@ -10,7 +10,6 @@
   # Locale & time
   time.timeZone = "Asia/Shanghai";
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings.LC_ALL = "zh_CN.UTF-8";
 
   # sudo 免密码（wheel 组）
   security.sudo = {
