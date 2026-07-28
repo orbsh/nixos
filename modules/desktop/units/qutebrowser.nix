8aa03a3dcbd0579a2d333086c66fb3f881b20434
orbit <fj0rd@qq.com>
2026-07-28 10:38:31 +0800

    fix: remove QT_WAYLAND_DISABLE_WINDOWDECORATION causing COSMIC server-side decorations

--- modules/desktop/units/qutebrowser.nix
+++ modules/desktop/units/qutebrowser.nix
@@ -1,15 +1,14 @@
 { pkgs, lib, user, ... }:
 
 let
-  # 强制 Wayland 原生 + 禁用窗口装饰（标题栏）
+  # 强制 Wayland 原生
   qutebrowser-wayland = pkgs.symlinkJoin {
     name = "qutebrowser-wayland";
     paths = [ pkgs.qutebrowser ];
     buildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/qutebrowser \
-        --set QT_QPA_PLATFORM wayland \
-        --set QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+        --set QT_QPA_PLATFORM wayland
     '';
   };
 in {
